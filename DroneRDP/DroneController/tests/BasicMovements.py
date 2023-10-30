703cbf69932e74e1dbeeedde2d0aa82f6f83e579
Gavin Kuehn <gdkuehn03@gmail.com>
2023-10-30 16:45:34 -0700

    DroneController 0.0.3

--- DroneRDP/DroneController/tests/BasicMovements.py
+++ DroneRDP/DroneController/tests/BasicMovements.py
@@ -7,6 +7,12 @@ from djitellopy import tello
 from time import sleep
 
 # Connect to the drone
-me = tello.Tello()
-me.connect()
-print(me.get_battery())
+tello = tello.Tello()
+tello.connect()
+print(tello.get_battery())
+
+
+tello.takeoff()
+tello.send_rc_control(0,50,0,0)
+sleep(3)
+tello.land()
